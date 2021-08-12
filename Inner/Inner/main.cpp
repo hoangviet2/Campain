bd339bbfe89c25e7d62adc173ef94f91bd47ede3
hoangviet2 <hoangviet18306@gmail.com>
2021-08-12 21:50:20 +0700

    execute file

---
+++ Inner/Inner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Inner
+//
+//  Created by Hoang Viet on 12/08/2021.
+//
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}

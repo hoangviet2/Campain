210413b148cadda53c0bcbb021726c7ba652e575
hoangviet2 <hoangviet18306@gmail.com>
2021-08-12 21:51:29 +0700

    Solution of 2013 files

---
+++ Inner/Inner/Olympic_10_2013_C/oplc13.cpp
@@ -0,0 +1,28 @@
+//
+//  oplc13.cpp
+//  Inner
+//
+//  Created by Hoang Viet on 12/08/2021.
+//
+
+#include "oplc13.hpp"
+void OlympicC2013::Node(string input, string output){
+    cout<<"Node test run"<<endl;
+    
+    cout<<"Node test out"<<endl;
+    return;
+}
+
+void OlympicC2013::Oil(string input, string output){
+    cout<<"Oil test run"<<endl;
+    
+    cout<<"Oil test out"<<endl;
+    return;
+}
+
+void OlympicC2013::Road(string input, string output){
+    cout<<"Road test run"<<endl;
+    
+    cout<<"Road test out"<<endl;
+    return;
+}
---
+++ Inner/Inner/Olympic_10_2013_C/oplc13.hpp
@@ -0,0 +1,23 @@
+//
+//  oplc13.hpp
+//  Inner
+//
+//  Created by Hoang Viet on 12/08/2021.
+//
+
+#ifndef oplc13_hpp
+#define oplc13_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+class OlympicC2013{
+public:
+    OlympicC2013(){cout<<"Begin test 2013 Gifted"<<endl;};
+    void Node(string,string);
+    void Oil(string,string);
+    void Road(string,string);
+    ~OlympicC2013();
+};
+#endif /* oplc13_hpp */
